32cf961ec0f2e01e3fec7f5f43e3736db367983a
Imtaek Hong <imtaekh@gmail.com>
2016-02-25 23:22:11 -0800

    collusion check updated

--- pongOnline/BallObject.js
+++ pongOnline/BallObject.js
@@ -1,6 +1,9 @@
 var SETTINGS = require("./SETTINGS.js");
 var BaseObejct = require("./BaseObject.js");
 
+var DIR = { RIGHT: 1, LEFT: -1};
+var COLLUSION_TYPE = { NO_COLLUSION: -1, VERTICAL: 1, HORIZONTAL: 2};
+
 function Ball(){
   BaseObejct.call(this);
   this.status.x = (SETTINGS.WIDTH-SETTINGS.BALL.WIDTH)/2;
@@ -18,61 +21,62 @@ Ball.prototype.constructor = Ball;
 Ball.prototype.update = function(objects){
   this.status.x += this.dx*this.speed;
   this.status.y += this.dy*this.speed;
-  if(this.status.x <= 0){
+  if(this.status.x <= 0)
     this.dx = Math.abs(this.dx);
-    console.log("changed");
-  }
-  if(this.status.x + this.status.width >= SETTINGS.WIDTH){
+  if(this.status.x + this.status.width >= SETTINGS.WIDTH)
     this.dx = -Math.abs(this.dx);
-    console.log("changed");
-  }
   if(this.status.y <= 0)
     this.dy = Math.abs(this.dy);
   if(this.status.y + this.status.height >= SETTINGS.HEIGHT)
     this.dy = -Math.abs(this.dy);
 
+  var headingTo = (this.dx>0)?(DIR.RIGHT):(DIR.LEFT);
   for(var object in objects){
     var status = objects[object].status;
     if(object != "ball"){
-      if(ballCollusionCheck(this.status, status)){
-        var type = ballCollusionTypeCheck(this.status, status, (this.dx>0)?(-1):(1));
-        switch(type){
-          case "vertical":
-            this.dy = bounce(this.status.y+this.status.height/2, status.y+status.height/2, this.dy);
-            break;
-          case "horizontal":
-            this.dx = bounce(this.status.x+this.status.width/2, status.x+status.width/2, this.dx);
-            break;
-        }
+      var collusionType = ballCollusionCheck(this.status, status, headingTo, Math.abs(this.dx));
+      switch(collusionType){
+        case COLLUSION_TYPE.NO_COLLUSION:
+          break;
+        case COLLUSION_TYPE.VERTICAL:
+          this.dy = bounce(this.status.y+this.status.height/2, status.y+status.height/2, this.dy);
+          break;
+        case COLLUSION_TYPE.HORIZONTAL:
+          this.dx = bounce(this.status.x+this.status.width/2, status.x+status.width/2, this.dx);
+          break;
       }
     }
   }
 };
 module.exports = Ball;
+
 function bounce (x, y, v){
-  if(x<y)
-    return -Math.abs(v);
-  else
-    return Math.abs(v);
-}
-function ballCollusionCheck(ball,player,dir){
-  if(!dir) dir = 0;
-  if(pointSquareCollusionCheck(dir+ball.x             , ball.y              , player))
-    return true;
-  if(pointSquareCollusionCheck(dir+ball.x + ball.width, ball.y              , player))
-    return true;
-  if(pointSquareCollusionCheck(dir+ball.x             , ball.y + ball.height, player))
-    return true;
-  if(pointSquareCollusionCheck(dir+ball.x + ball.width, ball.y + ball.height, player))
-    return true;
+  return x<y ? -Math.abs(v) : Math.abs(v);
 }
 
-function ballCollusionTypeCheck(ball,player,dir){
-  if(ballCollusionCheck(ball,player,dir)){
-    return "vertical";
-  } else {
-    return "horizontal";
+function ballCollusionCheck(ball,player,headingTo,xVelocity){
+  var adj = -headingTo * xVelocity; // previous x coordinate
+  if(pointSquareCollusionCheck(           ball.x             , ball.y              , player )){
+    return pointSquareCollusionCheck( adj+ball.x             , ball.y              , player )?
+      COLLUSION_TYPE.VERTICAL:
+      COLLUSION_TYPE.HORIZONTAL;
   }
+  if(pointSquareCollusionCheck(           ball.x + ball.width, ball.y              , player )){
+    return pointSquareCollusionCheck( adj+ball.x + ball.width, ball.y              , player )?
+      COLLUSION_TYPE.VERTICAL:
+      COLLUSION_TYPE.HORIZONTAL;
+  }
+  if(pointSquareCollusionCheck(           ball.x             , ball.y + ball.height, player )){
+    return pointSquareCollusionCheck( adj+ball.x             , ball.y + ball.height, player )?
+      COLLUSION_TYPE.VERTICAL:
+      COLLUSION_TYPE.HORIZONTAL;
+  }
+  if(pointSquareCollusionCheck(           ball.x + ball.width, ball.y + ball.height, player )){
+    return pointSquareCollusionCheck( adj+ball.x + ball.width, ball.y + ball.height, player )?
+      COLLUSION_TYPE.VERTICAL:
+      COLLUSION_TYPE.HORIZONTAL;
+  }
+  return COLLUSION_TYPE.NO_COLLUSION;
 }
 
 function pointSquareCollusionCheck(x,y,square){
